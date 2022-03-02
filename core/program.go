6db893e3f22f30ad85142f52f011b388faab8d7a
Esequiel Albornoz <esequielalbornoz7@gmail.com>
2022-03-02 09:01:04 -0300

    :fire: Remove unused code

--- core/program.go
+++ core/program.go
@@ -17,21 +17,6 @@ func (p *Program) Push(operation Operation) {
 	*p = append(*p, operation)
 }
 
-func (p *Program) Pop() (Operation, error) {
-	if p.IsEmpty() {
-		return nil, fmt.Errorf("can not perform `Program.Pop()`, program is empty.")
-	}
-
-	// Get the index of the top most element.
-	index := len(*p) - 1
-	// Index into the slice and obtain the element.
-	operation := (*p)[index]
-	// Remove it from the stack by slicing it off.
-	*p = (*p)[:index]
-
-	return operation, nil
-}
-
 func (p *Program) parseLines(lines []string) error {
 	var blocks BlockStack
 
