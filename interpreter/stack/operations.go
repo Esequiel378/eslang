467b8e240659f57c349bd8000d0b18b932637bdd
Esequiel Albornoz <esequielalbornoz7@gmail.com>
2022-03-26 19:49:04 -0300

    ⚡ Add `float` and `str` types to stack operations

--- interpreter/stack/operations.go
+++ interpreter/stack/operations.go
@@ -16,6 +16,10 @@ func AddValues(lhs StackValue, rhs StackValue) (StackValue, error) {
 	switch lhs.Type() {
 	case core.Int:
 		return NewStackValueInt(lhs.Value().(int64) + rhs.Value().(int64)), nil
+	case core.Float:
+		return NewStackValueFloat(lhs.Value().(float64) + rhs.Value().(float64)), nil
+	case core.String:
+		return NewStackValueString(lhs.Value().(string) + rhs.Value().(string)), nil
 	}
 
 	return nil, fmt.Errorf("can not add values of type %s", lhs.Type())
@@ -30,6 +34,8 @@ func SubtractValues(lhs StackValue, rhs StackValue) (StackValue, error) {
 	switch lhs.Type() {
 	case core.Int:
 		return NewStackValueInt(lhs.Value().(int64) - rhs.Value().(int64)), nil
+	case core.Float:
+		return NewStackValueFloat(lhs.Value().(float64) - rhs.Value().(float64)), nil
 	}
 
 	return nil, fmt.Errorf("can not subtract values of type %s", lhs.Type())
@@ -44,6 +50,8 @@ func MultiplyValues(lhs StackValue, rhs StackValue) (StackValue, error) {
 	switch lhs.Type() {
 	case core.Int:
 		return NewStackValueInt(lhs.Value().(int64) * rhs.Value().(int64)), nil
+	case core.Float:
+		return NewStackValueFloat(lhs.Value().(float64) * rhs.Value().(float64)), nil
 	}
 
 	return nil, fmt.Errorf("can not multiply values of type %s", lhs.Type())
@@ -55,9 +63,20 @@ func CompareEqualValues(lhs StackValue, rhs StackValue) (StackValue, error) {
 		return nil, fmt.Errorf("can not compare values of different types")
 	}
 
+	// TODO: create a StackValueBoo type
 	switch lhs.Type() {
 	case core.Int:
 		if lhs.Value().(int) == rhs.Value().(int) {
+			return NewStackValueInt(1), nil
+		}
+		return NewStackValueInt(0), nil
+	case core.Float:
+		if lhs.Value().(float64) == rhs.Value().(float64) {
+			return NewStackValueInt(1), nil
+		}
+		return NewStackValueInt(0), nil
+	case core.String:
+		if lhs.Value().(string) == rhs.Value().(string) {
 			return NewStackValueInt(1), nil
 		}
 		return NewStackValueInt(0), nil
